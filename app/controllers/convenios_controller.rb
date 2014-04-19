6707550650b0626516a72c35236262ee64c09d6a
Daniel Henrique <danielhmarinho@Daniel.notebook>
2014-04-19 15:52:25 -0300

    Adicionando controller de Convenios

---
+++ app/controllers/convenios_controller.rb
@@ -0,0 +1,2 @@
+class ConveniosController < ApplicationController
+end
---
+++ test/controllers/convenios_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ConveniosControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
