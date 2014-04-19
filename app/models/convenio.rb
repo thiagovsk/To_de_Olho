97a6efb5ce12cc6cfd884c086010a4c8cf739bf7
AlessandroCaetano <alebr28@hotmail.com>
2014-04-19 15:35:14 -0300

    Adicionando Modelo de Convenios e Método de Importação de CSV

---
+++ app/models/convenio.rb
@@ -0,0 +1,8 @@
+class Convenio < ActiveRecord::Base
+
+	def self.import(file)
+		CSV.foreach(file.path, headers: true, encoding: "iso-8859-1:UTF-8") do |row|
+			Convenio.create! row.to_hash
+		end
+	end
+end
---
+++ db/migrate/20140419182234_create_convenios.rb
@@ -0,0 +1,29 @@
+class CreateConvenios < ActiveRecord::Migration
+  def change
+    create_table :convenios do |t|
+      t.string :numeroconvenio
+      t.string :uf
+      t.string :codigosiafi
+      t.string :situacaoconvenio
+      t.string :numerooriginal
+      t.string :objetoconvenio
+      t.string :codigoorgaosuperior
+      t.string :nomeorgaosuperior
+      t.string :codigoconcedente
+      t.string :nomeconcedente
+      t.string :codigoconvenente
+      t.string :nomeconvenente
+      t.string :tipoentenconvenente
+      t.string :valorconvenio
+      t.string :valorliberado
+      t.string :datapublicacao
+      t.string :datainiciovigencia
+      t.string :datafimvigencia
+      t.string :valorcontrapartida
+      t.string :dataultimaliberacao
+      t.string :valorultimaliberacao
+
+      t.timestamps
+    end
+  end
+end
