011b7deaa44895b09a6cd97328daba9885e20725
Daniel Henrique <danielhmarinho@Daniel.notebook>
2014-04-19 16:22:52 -0300

    Adicionando metodo de acesso aos atributos encapsulados

--- app/controllers/convenios_controller.rb
+++ app/controllers/convenios_controller.rb
@@ -1,2 +1,17 @@
 class ConveniosController < ApplicationController
+
+private
+
+def set_convenio
+@convenio = Convenio.find(params[:id])
 end
+ 
+
+def convenio_params
+params.require(:convenio).permit(:numeroconvenio, :uf, :codigosiafi, :nomemunicipio, :situacaoconvenio, :numerooriginal, :objetoconvenio, :codigoorgaosuperior, :nomeorgaosuperior, :codigoconcedente, :nomeconcedente, :codigoconvenente, :nomeconvenente, :tipoenteconvenente, :valorconvenio, :valorliberado, :datapublicacao, :datainiciovigencia, :datafimvigencia, :valorcontrapartida, :dataultimaliberacao, :valorultimaliberacao)
+end
+ 
+end
+
+
+
