4bd1d6bfa557beb145e92a8318eb4b5cc6eb6269
Daniel Henrique <danielhmarinho@gmail.com>
2014-04-29 15:40:27 -0300

    Adicionando rotas e layout de alerta para o devise

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   end
 
   resources :usuarios
+  root :to => "home#index"
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
