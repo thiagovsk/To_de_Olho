5b4256f1b3434be8ca021676b9bc4ab8c26d4da4
victorcabeceira <victorfgcabeceira@gmail.com>
2014-04-29 15:53:22 -0300

    Correção testes

--- spec/controllers/usuarios_controller_spec.rb
+++ spec/controllers/usuarios_controller_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe UsuariosController do
 
-  let(:valid_attributes) { { :nome  => "Alessandro", :cpf => "123456789", :email => "asdwer@gmail.com", :login => "abcdewerwe", :senha => "abcsdfdsw"  } }
+  let(:valid_attributes) { { :nome  => "Victor", :cpf => "12345678900", :email => "asdwer@gmail.com", :login => "abcdewerwe", :senha => "abcsdfdsw"  } }
 
-  let(:valid_session) { {} }
+  let(:valid_session) {  }
 
   describe "GET index" do
     it "assigns all usuarios as @usuarios" do
       usuario = Usuario.create! valid_attributes
       get :index, {}, valid_session
-      assigns(:usuarios).should eq([usuario])
+      expect(assigns(:usuarios)).to eq([usuario])
     end
   end
 
   describe "GET new" do
     it "assigns a new usuario as @usuario" do
       get :new, {}, valid_session
-      assigns(:usuario).should be_a_new(Usuario)
+      expect(assigns(:usuarios)).to be_a_new(Usuario)
     end
   end
 
@@ -32,8 +32,8 @@ describe UsuariosController do
 
       it "assigns a newly created usuario as @usuario" do
         post :create, {:usuario => valid_attributes}, valid_session
-        assigns(:usuario).should be_a(Usuario)
-        assigns(:usuario).should be_persisted
+        expect(assigns(:usuarios)).to be_a(Usuario)
+        assigns(:usuarios).should be_persisted
       end
 
       it "redirects to the created usuario" do
@@ -45,14 +45,14 @@ describe UsuariosController do
     describe "with invalid params" do
       it "assigns a newly created but unsaved usuario as @usuario" do
         Usuario.any_instance.stub(:save).and_return(false)
-        post :create, {:usuario => {  }}, valid_session
-        assigns(:usuario).should be_a_new(Usuario)
+        post :create, {:usuarios => {  }}, valid_session
+        expect(assigns(:usuarios)).to be_a_new(Usuario)
       end
 
       it "re-renders the 'new' template" do
         Usuario.any_instance.stub(:save).and_return(false)
         post :create, {:usuario => {  }}, valid_session
-        response.should render_template("new")
+        expect(response).to render_template("new")
       end
     end
   end
