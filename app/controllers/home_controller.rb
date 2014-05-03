7f59681a456a6e44f231bc48dd17f9d89b8d58fb
AlessandroCaetano <alessandro.caetanob@gmail.com>
2014-05-03 18:36:21 -0300

    Criação do método show  na Home Controller e alteração do método index, criação da partial show de home e implementação JS para renderização da mesma

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
-def index
-end
+	def index
+		@usuarios = Usuario.all
+	end
+
+	def show
+  		@usuario = Usuario.find(params[:id])
+
+		respond_to do |format|
+   			format.js { render :show }
+		end
+	end 
+
 end
