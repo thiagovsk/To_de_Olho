18371d939757451b2813e2ddd1f8e451f6887d1d
thiagovsk <thiagitosouza@hotmail.com>
2014-05-14 21:19:37 -0300

    Update spec_helper.rb
    Adicionando coveralls para contagem da cobertura.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'simplecov'
+require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    Coveralls::SimpleCov::Formatter,
+    SimpleCov::Formatter::HTMLFormatter,
+]
 SimpleCov.start
 
 
