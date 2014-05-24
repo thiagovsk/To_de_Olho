f6c3d984aae478d986988a26aa7ddfb8afc2fc99
victorcabeceira <victorfgcabeceira@gmail.com>
2014-05-24 15:07:09 -0300

    Criacao dos testes da controller de convenios

---
+++ spec/controllers/convenios_controller_spec.rb
@@ -0,0 +1,35 @@
+require 'spec_helper'
+
+describe ConveniosController do
+
+  let(:valid_attributes) { {:id => 1, :numeroconvenio => "900037",
+   :uf => "SC", :codigosiafi => "8255", :nomemunicipio => "Brasilia",
+   :situacaoconvenio => "Em execucao", :numerooriginal => "05520/2009",
+   :objetoconvenio => "Pavimentacao", :codigoorgaosuperior => "22000",
+   :nomeorgaosuperior => "MINIST. DA AGRICUL..PECUARIA E ABASTECIMENTO ",
+   :codigoconcedente => "135098", :nomeconcedente => "CAIXA ECONOMICA FEDERAL/MA ",
+   :codigoconvenente => "82827148000169", :nomeconvenente => "MUNICIPIO DE PINHEIRO PRETO ",
+   :tipoenteconvenente => "'M' (municipal)", :valorconvenio => "97500.00",
+   :valorliberado => "97500.00", :datapublicacao => "2010-01-28", :datainiciovigencia => "2009-12-31",
+   :datafimvigencia => "2014-12-31 ", :valorcontrapartida => "19690.29 ",
+   :dataultimaliberacao => "2011-12-28", :valorultimaliberacao => "97500.00",
+   :created_at => "2010-10-20", :updated_at => "2013-10-25"} }
+
+  let(:valid_session) { {} }
+
+  describe "GET index" do
+    it "assigns all convenios as @convenios" do
+      convenios = Convenio.create! valid_attributes
+      get :index, {}, valid_session
+      assigns(:convenios).should eq([convenios])
+    end
+  end
+
+  describe "GET new" do
+    it "assigns a new convenio as @convenio" do
+      get :new, {}, valid_session
+      assigns(:convenios).should be_a_new(Convenio)
+    end
+  end
+
+end
