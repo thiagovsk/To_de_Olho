9f6320acafb29b42fd1ef9a8fdb139f3b4565390
Ricardo <ricardocdzg@hotmail.com>
2014-05-31 14:36:33 -0300

    Criando associação entre reclamação e convenio

--- app/controllers/reclamacao_controller.rb
+++ app/controllers/reclamacao_controller.rb
@@ -1,2 +1,52 @@
+# -*- encoding : utf-8 -*-
 class ReclamacaoController < ApplicationController
+	 before_action :set_reclamacao , only:[:show, :edit, :destroy, :update]
+
+  def index
+    @reclamacoes = Reclamacao.all
+  end
+
+  def new
+    @reclamacoes = Reclamacao.new
+  end
+
+  def create
+    @reclamacoes = Reclamacao.new(reclamacao_params)
+    respond_to do |format|
+      if @reclamacoes.save
+        format.html { redirect_to reclamacoes_path, notice: 'Usuário Cadastrado com sucesso' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+
+  def destroy
+    @reclamacoes.destroy
+    respond_to do |format|
+      format.html { redirect_to reclamacoes_path, notice: 'Deletado com sucesso' }
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @reclamacoes.update(reclamacao_params)
+        format.html { redirect_to reclamacoes_path, notice: 'Alteração realizada com sucesso' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+  def set_reclamacao
+    @reclamacoes = Reclamacao.find(params[:id])
+  end
+
+  def Reclamacao_params
+    params.fetch(:reclamacao, {}).permit(:reclamacao)
+    #Ao invés de utilizar params.require, utilize params.fetch
+    #Esse problema é intrínseco ao Rails e é solucionado com o fetch
+  end
 end
