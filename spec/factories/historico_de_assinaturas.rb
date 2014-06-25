d3287eb2e57209ff1b602c5b3d308f7bf78911cd
Andre Lucas Verissimo de Souza <lukkalavero@yahoo.com>
2014-06-25 14:05:52 -0400

    Criando testes que faltavam das models

--- spec/factories/historico_de_assinaturas.rb
+++
@@ -1,7 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :historico_de_assinatura do
-    assinatura nil
-  end
-end
--- spec/models/abaixo_assinado_spec.rb
+++ spec/models/abaixo_assinado_spec.rb
@@ -1,5 +1,12 @@
+require 'spec_helper'
 
+describe AbaixoAssinado do
+  it "Pode ser instanciado" do
+    AbaixoAssinado.new.should be_an_instance_of(AbaixoAssinado)
+  end
 
-RSpec.describe AbaixoAssinado, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:assinaturas) }
+  it { should belong_to(:usuario) }
+  it { should belong_to(:convenio) }
+	
 end
