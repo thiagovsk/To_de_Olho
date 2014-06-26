68789afb3e3f2dbd99015d468e523faf0d9d2161
Andre Lucas Verissimo de Souza <lukkalavero@yahoo.com>
2014-06-26 00:15:37 -0300

    resolvendo problema de deprecation

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,7 +12,7 @@ SimpleCov.start
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'rspec/autorun'
+#require 'rspec/autorun'
 require 'factory_girl_rails'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
